13986b3995326f5e7c05cba1da8367ac8186c540
HaviLee <Havi.X.Li.-ND@disney.com>
2018-12-03 13:24:31 +0800

    add new image

--- iOS底层原理总结/ViewController.m
+++ iOS底层原理总结/ViewController.m
@@ -29,6 +29,23 @@
 	if (class_isMetaClass(meta_class1)) {
 		NSLog(@"is meta-class");
 	}
+	
+	//
+	NSObject *object1 = [[NSObject alloc] init];
+	NSObject *object2 = [[NSObject alloc] init];
+	NSLog(@"%p %p", object1, object2);
+	Class objectClass1 = [object1 class];
+	Class objectClass2 = [object2 class];
+	Class objectClass3 = [NSObject class];
+	// runtime
+	Class objectClass4 = object_getClass(object1);
+	Class objectClass5 = object_getClass(object2);
+	NSLog(@"%p %p %p %p %p", objectClass1, objectClass2, objectClass3, objectClass4, objectClass5);
+	
+	// runtime
+	Class metaObjectClass1 = object_getClass([NSObject class]);
+	Class metaObjectClass2 = [NSObject class];
+	NSLog(@"%p %p", metaObjectClass1, metaObjectClass2);
 
 }
 
